3b564a305e36966bd1a7fda49e0afca0fb5ce11d
Dawid <dawid.gredzinski24@gmail.com>
2022-06-07 00:42:17 +0200

    next step for 4

--- app/src/main/java/com/example/niedziala/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/niedziala/ui/main/MainFragment.kt
@@ -1,18 +1,23 @@
 package com.example.niedziala.ui.main
 
 import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
 import com.example.niedziala.R
 import com.example.niedziala.databinding.MainFragmentBinding
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), SensorEventListener {
 
     companion object {
         fun newInstance() = MainFragment()
@@ -42,14 +47,59 @@ class MainFragment : Fragment() {
         if (container != null) {
             thiscontext = container.context
         }
-        getManager()
+
+        if(!viewModel.isSensorManagerInitialized()){
+            getManager()
+        }
 
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (viewModel.isLightSensorInitialized()){
+            viewModel.mSensorManager.registerListener(this,viewModel.mLightSensor,SensorManager.SENSOR_DELAY_UI)
+            Log.i("Light","init")
+        }
+        else{
+            Log.i("Light","not init")
+        }
+        if (viewModel.isProximitySensorInitialized()){
+            viewModel.mSensorManager.registerListener(this,viewModel.mProximitySensor,SensorManager.SENSOR_DELAY_UI)
+            Log.i("Proximity","init")
+        }
+        else
+            Log.i("Proximity","not init")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        viewModel.mSensorManager.unregisterListener(this)
     }
 
     private fun getManager(){
         viewModel.mSensorManager = getActivity()?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         viewModel.getSensors()
+        viewModel.mLightSensor = viewModel.mSensorManager.getDefaultSensor(5)
+        viewModel.mProximitySensor = viewModel.mSensorManager.getDefaultSensor(8)
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        var sensorType = p0!!.sensor.type
+        if(sensorType == Sensor.TYPE_LIGHT){
+            var currVal = p0.values[0]
+            viewModel.setLight(currVal)
+            Log.i("Light",currVal.toString())
+        }
+        if(sensorType == Sensor.TYPE_PROXIMITY){
+            var currVal = p0.values[0]
+            viewModel.setDistance(currVal)
+            Log.i("Proximity",currVal.toString())
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
     }
 
 }
--- app/src/main/java/com/example/niedziala/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/niedziala/ui/main/MainViewModel.kt
@@ -9,13 +9,27 @@ import androidx.lifecycle.ViewModel
 class MainViewModel : ViewModel() {
 
     var _resText: MutableLiveData<String> = MutableLiveData("Your sensors will appear here")
-
     val resText: LiveData<String>
         get() = _resText
 
+    var _resLight: MutableLiveData<String> = MutableLiveData("Sensors not exist")
+    val resLight: LiveData<String>
+        get() = _resLight
+
+    var _resDistance: MutableLiveData<String> = MutableLiveData("Sensors not exist")
+    val resDistance: LiveData<String>
+        get() = _resDistance
+
     lateinit var mSensorManager: SensorManager
+    lateinit var mLightSensor: Sensor
+    lateinit var mProximitySensor: Sensor
 
     var sensors = mutableListOf<Sensor>()
+
+    fun isSensorManagerInitialized() = ::mSensorManager.isInitialized
+    fun isLightSensorInitialized() = ::mLightSensor.isInitialized
+    fun isProximitySensorInitialized() = ::mProximitySensor.isInitialized
+
 
     fun getSensors(){
         sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL)
@@ -27,6 +41,14 @@ class MainViewModel : ViewModel() {
         }
 
         _resText = MutableLiveData(resWord)
+    }
+
+    fun setLight(s: Float){
+        _resLight = MutableLiveData(s.toString())
+    }
+
+    fun setDistance(s: Float){
+        _resDistance = MutableLiveData(s.toString())
     }
 
 }
